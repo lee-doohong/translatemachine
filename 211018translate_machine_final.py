ce82f65909c42c2a7b779089e8cc1d0fa39904cc
lee-doohong <lovelyldh@naver.com>
2021-10-18 23:12:39 +0900

    fixed

--- 211018translate_machine_final.py
+++ 211018translate_machine_final.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import codecs
-import os
+import os #필요없는 코드
 
 driver = webdriver.Chrome(r'C:\Users\admin\crawlingtwitter\chromedriver')
 
